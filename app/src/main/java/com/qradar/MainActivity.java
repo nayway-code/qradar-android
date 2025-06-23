83bdc5c579f5a6d55544d83bc85e192bb5d806ac
nayway-code <nayway70@gmail.com>
2025-06-23 20:47:59 +0630

    Update MainActivity.java

--- app/src/main/java/com/qradar/MainActivity.java
+++ app/src/main/java/com/qradar/MainActivity.java
@@ -1,1 +1,29 @@
- 
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main); // you'll create this XML later
+
+        // Overlay permission check
+        if (!Settings.canDrawOverlays(this)) {
+            Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                Uri.parse("package:" + getPackageName()));
+            startActivityForResult(intent, 1234);
+        }
+
+        // Start VPN
+        findViewById(R.id.btnStart).setOnClickListener(v -> {
+            Intent prepare = VpnService.prepare(this);
+            if (prepare != null) {
+                startActivityForResult(prepare, 5678);
+            } else {
+                startServices();
+            }
+        });
+    }
+
+    private void startServices() {
+        startService(new Intent(this, RadarVpnService.class));
+        startService(new Intent(this, OverlayService.class));
+    }
+}
